09d8311659d9cec52c8988d92a498cc1b8074102
SajalSood <sajal.sood@gmail.com>
2021-02-15 12:45:35 -0500

    added sentiment analysis logic

--- sa-logic/sa/sentiment_analysis.py
+++ sa-logic/sa/sentiment_analysis.py
@@ -1,4 +1,22 @@
-flask
-flask-cors 
-requests 
-textblob==0.15.0
+from textblob import TextBlob
+from flask import Flask, request, jsonify 
+from flask_cors import CORS
+
+app = Flask(__name__) 
+CORS(app)
+
+@app.route("/testHealth") 
+    def hello():
+    return "Hello World!"
+
+@app.route("/analyse/sentiment", methods=['POST']) 
+    def analyse_sentiment():
+        sentence = request.get_json()['sentence']
+        polarity = TextBlob(sentence).sentences[0].polarity 
+        return jsonify(
+            sentence=sentence,
+            polarity=polarity 
+        )
+
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5000)
