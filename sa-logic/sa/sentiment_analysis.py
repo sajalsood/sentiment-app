6f8de4f249b5c936ad57b0bb4ab9f599396d16f8
SajalSood <sajal.sood@gmail.com>
2021-02-15 12:48:37 -0500

    added server logic

--- sa-logic/sa/sentiment_analysis.py
+++ sa-logic/sa/sentiment_analysis.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/testHealth") 
-    def hello():
+def hello():
     return "Hello World!"
 
 @app.route("/analyse/sentiment", methods=['POST']) 
-    def analyse_sentiment():
-        sentence = request.get_json()['sentence']
-        polarity = TextBlob(sentence).sentences[0].polarity 
-        return jsonify(
-            sentence=sentence,
-            polarity=polarity 
-        )
+def analyse_sentiment():
+    sentence = request.get_json()['sentence']
+    polarity = TextBlob(sentence).sentences[0].polarity 
+    return jsonify(
+        sentence=sentence,
+        polarity=polarity 
+    )
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5000)
